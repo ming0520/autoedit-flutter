89d141c68e683b12df87405e28dc93a4bdaa898c
Tan Zhong Ming <mingblog0520@gmail.com>
2021-01-12 00:39:50 +0800

    Initial commit
    Feature done:
    -import and record button
    -video player
    
    Worry:
    maybe video player with chewie not doing well after select many video.

---
+++ android/app/src/main/java/com/zhongming/autoedit/MainActivity.java
@@ -0,0 +1,6 @@
+package com.zhongming.autoedit;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/useless/camera_screen2.dart
@@ -0,0 +1,122 @@
+//import 'dart:io';
+//
+//import 'package:camera/camera.dart';
+//import 'package:flutter/material.dart';
+//import 'package:open_file/open_file.dart';
+//import 'package:path_provider/path_provider.dart';
+//
+//class CameraScreen extends StatefulWidget {
+//  @override
+//  _CameraScreenState createState() => _CameraScreenState();
+//}
+//
+//class _CameraScreenState extends State<CameraScreen> {
+//  List<CameraDescription> _cameras;
+//  CameraController _controller;
+//  int _cameraIndex;
+//  bool _isRecording = false;
+//  String _filePath;
+//
+//  @override
+//  void initState() {
+//    super.initState();
+//    availableCameras().then((cameras) {
+//      _cameras = cameras;
+//      if (_cameras.length != 0) {
+//        _cameraIndex = 0;
+//        _initCamera(_cameras[_cameraIndex]);
+//      }
+//    });
+//  }
+//
+//  _initCamera(CameraDescription camera) async {
+//    if (_controller != null) await _controller.dispose();
+//    _controller = CameraController(camera, ResolutionPreset.medium);
+//    _controller.addListener(() => this.setState(() {}));
+//    _controller.initialize();
+//  }
+//
+//  Widget _buildCamera() {
+//    if (_controller == null || !_controller.value.isInitialized)
+//      return Center(child: Text('Loading...'));
+//    return AspectRatio(
+//      aspectRatio: _controller.value.aspectRatio,
+//      child: CameraPreview(_controller),
+//    );
+//  }
+//
+//  Widget _buildControls() {
+//    return Row(
+//      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//      children: <Widget>[
+//        IconButton(
+//          icon: Icon(_getCameraIcon(_cameras[_cameraIndex].lensDirection)),
+//          onPressed: _onSwitchCamera,
+//        ),
+//        IconButton(
+//          icon: Icon(Icons.radio_button_checked),
+//          onPressed: _isRecording ? null : _onRecord,
+//        ),
+//        IconButton(
+//          icon: Icon(Icons.stop),
+//          onPressed: _isRecording ? _onStop : null,
+//        ),
+//        IconButton(
+//          icon: Icon(Icons.play_arrow),
+//          onPressed: _isRecording ? null : _onPlay,
+//        ),
+//      ],
+//    );
+//  }
+//
+//  void _onPlay() => OpenFile.open(_filePath);
+//
+//  Future<void> _onStop() async {
+//    await _controller.stopVideoRecording();
+//    setState(() => _isRecording = false);
+//  }
+//
+//  Future<void> _onRecord() async {
+////    var directory = await getTemporaryDirectory();
+////    _filePath = directory.path + '/${DateTime.now()}.mp4';
+////    _controller.startVideoRecording(_filePath);
+////    setState(() => _isRecording = true);
+//    final Directory appDir = await getApplicationDocumentsDirectory();
+//    final String videoDir = '${appDir.path}/Videos';
+//    await Directory(videoDir).create(recursive: true);
+//    final String currentTime = DateTime.now().millisecondsSinceEpoch.toString();
+//    final String filePath = '$videoDir/${currentTime}.mp4';
+//    try {
+//      await _controller.startVideoRecording(filePath);
+//      setState(() => _isRecording = true);
+//      _filePath = filePath;
+//    } on CameraException catch (e) {
+//      print(e);
+//
+//      return null;
+//    }
+//  }
+//
+//  IconData _getCameraIcon(CameraLensDirection lensDirection) {
+//    return lensDirection == CameraLensDirection.back
+//        ? Icons.camera_front
+//        : Icons.camera_rear;
+//  }
+//
+//  void _onSwitchCamera() {
+//    if (_cameras.length < 2) return;
+//    _cameraIndex = (_cameraIndex + 1) % 2;
+//    _initCamera(_cameras[_cameraIndex]);
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(title: Text('Video recording with Flutter')),
+//      body: Column(children: [
+//        Container(height: 500, child: Center(child: _buildCamera())),
+//        _buildControls(),
+//      ]),
+//    );
+//  }
+//}
