147a69e63d17183d75bcb5edc58913e3e4d7c104
Tan Zhong Ming <mingblog0520@gmail.com>
2021-01-13 23:39:06 +0800

    Update main.dart
    remove File video = 'path' that for testing purpose

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-//  File video = File('/storage/emulated/0/DCIM/Camera/VID20210111000649.mp4');
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
