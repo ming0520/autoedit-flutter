fe692971ebde92d551a4e7ddc8af371f50d8481c
Tan Zhong Ming <mingblog0520@gmail.com>
2021-02-05 22:09:24 +0800

    Create constant.dart
    -move out the spinkit

---
+++ lib/constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+final SPINKIT = SpinKitDualRing(
+  color: Colors.blue,
+  size: 175.0,
+  lineWidth: 13.0,
+);
